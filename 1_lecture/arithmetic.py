da17353de72ebcf6d71412d110ac2e006a52863d
maxlukyrabbit <maxlukyrabbit@yandex.ru>
2025-02-05 19:30:26 +0300

    lecture_2

---
+++ 1_lecture/arithmetic.py
@@ -0,0 +1,6 @@
+a = int(input("Первое число: "))
+b = int(input("Второе число: "))
+if(b == 0):
+	print(f"{a + b} {a - b} {a * b} n/a")
+else:
+	print(f"{a + b} {a - b} {a * b} {a / b}")
---
+++ 1_lecture/radius.py
@@ -0,0 +1,7 @@
+x = float(input("Введите x: "))
+y = float(input("Введите y: "))
+if(x**2 + y**2 < 25):
+	print("GOTCHA")
+else:
+	print("MISS")
+
